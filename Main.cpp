894b112dab8902a564a6721e425ac6c8d7bb3f7e
Kajani Shanmuganathan <kajani28@shan24.net>
2025-01-17 18:57:38 +0100

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+   int distance
+   string name
+   int duration
+   
+   cout << "enter your name" << endl;
+   cin >> name >> endl;
+   
+   cout << "enter distance of your run"
+   cin >> distance >> endl;
+   
+   cout << "enter duration of your run" << endl;
+   cin >> duration >> endl;
+   
+}
